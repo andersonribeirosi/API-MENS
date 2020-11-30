0d66181349c74b481554280871acd2deadec1e42
ArenaDev2 <dev2@arenafantasy.com>
2020-11-30 17:31:32 -0300

    Schem e model de casas

---
+++ API-DevHouse/src/models/House.js
@@ -0,0 +1,13 @@
+import {Schema, model } from 'mongoose';
+
+const HouseSchema = new Schema({
+    thumbnail: String,
+    description: String,
+    price: Number,
+    location: String,
+    status: Boolean,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
